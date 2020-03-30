7c1e1de3996a6eeffc55fb0f9e22a45293240c72
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-03-30 14:30:24 -0400

    Projeto Cara ou Coroa

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:caraoucoroa/Home.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
